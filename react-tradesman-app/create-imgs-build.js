a9970bddd5eb3c194e5d352366c1402b5fa478cc
Jordan Paisley <jordan@cypher.dev>
2021-06-17 12:45:58 +0100

    create post build workflow for IMGS

--- react-tradesman-app/create-imgs-build.js
+++ react-tradesman-app/create-imgs-build.js
@@ -1,20 +1,39 @@
 
 const glob = require('glob');
-const fs = require('fs');
+const fs = require('fs').promises;
 const { parse: parseHTML } = require('node-html-parser');
+const { html: beautifyHTML } = require('js-beautify');
+const rimraf = require('rimraf');
+const path = require('path');
 
-const BUILD_FILES = glob.sync('build/**/*.html');
+const HTML_BUILD_FILES = glob.sync('build/**/*.html');
+const JS_FILES_DIRECTORY = path.resolve('./build/static/js');
 
-const [trial] = BUILD_FILES.map(
-  (path) => {
-    const contents = fs.readFileSync(
+const shippableHTMLCode = HTML_BUILD_FILES.map(
+  async (path) => {
+    const contents = await fs.readFile(
       path, {encoding: 'utf-8'}
     );
     const html = parseHTML(contents);
-    html.querySelectorAll('script').forEach(t => t.remove());
+    html
+      .querySelectorAll('script')
+      .forEach(t => t.remove());
+      
+    const shippableHTML = beautifyHTML(
+      html.toString(),
+      { indent_size: 2 }
+    );
 
-    return html.toString();
+    return fs.writeFile(path, shippableHTML);
   }
 );
 
-console.log(trial)
+// remove js files
+rimraf.sync(JS_FILES_DIRECTORY);
+
+//run for all files
+Promise.all(
+  shippableHTMLCode,
+);
+
+
